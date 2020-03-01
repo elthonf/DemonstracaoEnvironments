d8afac722615b18db595ba833982649d5aa4af07
Elthon Freitas <elthonf@gmail.com>
2020-03-01 15:36:45 -0300

    Versao inicial

---
+++ Pacote01/main01.py
@@ -0,0 +1,6 @@
+import sys
+
+if __name__ == "__main__":
+    print("Olá, sou o pacote 01.")
+    print("Estou rodando sob a seguinte versão de Python:")
+    print(sys.version)
---
+++ Pacote02/main02.py
@@ -0,0 +1,6 @@
+import sys
+
+if __name__ == "__main__":
+    print 'Ola, sou o pacote 02.'
+    print 'Estou rodando sob a seguinte versao de Python:'
+    print sys.version
