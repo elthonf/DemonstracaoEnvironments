9a01b23c56837ddac87c77137aaeca4a6c04f03c
Elthon Freitas <elthonf@gmail.com>
2020-03-01 16:13:50 -0300

    Ajuste para exercício

--- Pacote03/main03.py
+++ Pacote03/main03.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 
 if __name__ == "__main__":
-    print("Olá, sou o pacote 01.")
+    print("Olá, sou o pacote 03.")
     print("Estou rodando sob a seguinte versão de Python:")
     print(sys.version)
     print("Versão do numpy:")
