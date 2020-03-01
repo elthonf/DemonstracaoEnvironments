ea39257b8f0c9ecd4906184a4d6e035b29037d1e
Elthon Freitas <elthonf@gmail.com>
2020-03-01 15:59:23 -0300

    Ajuste exercício

--- Pacote01/main01.py
+++ Pacote01/main01.py
@@ -4,3 +4,7 @@ if __name__ == "__main__":
     print("Olá, sou o pacote 01.")
     print("Estou rodando sob a seguinte versão de Python:")
     print(sys.version)
+
+    print("\n")
+    print("Modulos disponíveis:")
+    help("modules")
