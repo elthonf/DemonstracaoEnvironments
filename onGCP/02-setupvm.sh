6ff9f4e330977b52dd952e561a6f3c1b3e45fec1
Elthon Freitas <elthonf@gmail.com>
2020-06-01 13:44:44 -0300

    Setup ANACONDA

--- onGCP/02-setupvm.sh
+++ onGCP/02-setupvm.sh
@@ -5,6 +5,7 @@ echo "Install Anaconda"
 anaconda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 cd /tmp
 curl -O $anaconda_url
-sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+#sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 #Restart terminal
 source ~/.bashrc
