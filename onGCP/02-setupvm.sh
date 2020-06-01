d553bd4f000e353595f4e45d78de600a2ddb3f6c
Elthon Freitas <elthonf@gmail.com>
2020-06-01 15:25:03 -0300

    Ajuste no nome do Firewall para evitar dor de cabeça em aulas futuras

--- onGCP/02-setupvm.sh
+++ onGCP/02-setupvm.sh
@@ -1,6 +1,10 @@
 #anaconda_url=https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
 
 
+echo "Install programas uteis"
+sudo apt-get -y install tree
+
+
 echo "Install Anaconda"
 anaconda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 cd /tmp
