fcd68161959482a1ef4a412663029aedd89101bb
Elthon Freitas <elthonf@gmail.com>
2020-06-01 15:09:16 -0300

    Ajuste no setup da VM

--- onGCP/03-createEnvs.sh
+++ onGCP/03-createEnvs.sh
@@ -1,3 +1,5 @@
+https://github.com/elthonf/DemonstracaoEnvironments.git
+
 echo "Este script não faz parte do exercício, apenas das respostas."
 conda create -n myenv01A -y python=3.6
 conda create -n myenv01B -y python=2.7
@@ -9,3 +11,29 @@ conda env list
 
 echo "To change to a env, use 'conda activate name'"
 
+
+# Instala pacotes
+
+conda activate myenv01A
+pip list
+pip install -r Pacote01/requirements.txt
+
+conda activate myenv01B
+pip list
+pip install -r Pacote02/requirements.txt
+
+conda activate myenv01C
+pip list
+pip install -r Pacote03/requirements.txt
+
+
+# Testa / exec modulos
+
+conda activate myenv01A
+python Pacote01/main01.py
+
+conda activate myenv01B
+python Pacote02/main02.py
+
+conda activate myenv01C
+python Pacote03/main03.py
