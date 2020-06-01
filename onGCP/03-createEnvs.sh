5f46e0d301335aee3b912010d46af194f683fa82
Elthon Freitas <elthonf@gmail.com>
2020-06-01 14:45:24 -0300

    Respostas

---
+++ onGCP/03-createEnvs.sh
@@ -0,0 +1,11 @@
+echo "Este script não faz parte do exercício, apenas das respostas."
+conda create -n myenv01A -y python=3.6
+conda create -n myenv01B -y python=2.7
+conda create -n myenv01C -y python=3.7
+
+echo "##### ##### ##### ##### ##### #####"
+echo "To check envs, use 'conda env list'"
+conda env list
+
+echo "To change to a env, use 'conda activate name'"
+
