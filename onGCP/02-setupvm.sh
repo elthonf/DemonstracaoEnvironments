cf530bc6405393267e3d4a82bb37bef51318f159
Elthon Freitas <elthonf@gmail.com>
2020-06-01 14:46:24 -0300

    Ajuste no setup da VM

--- onGCP/02-setupvm.sh
+++ onGCP/02-setupvm.sh
@@ -8,7 +8,7 @@ curl -O $anaconda_url
 #sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 bash Miniconda3-latest-Linux-x86_64.sh
 
-#Altera owner da pasta
+#Altera owner da pasta, apenas se a instalação for feita pelo sudo
 #sudo chown -R 1001:1002 $HOME/.conda
 
 #Restart terminal (não funcionando, tem que fazer na mão)
